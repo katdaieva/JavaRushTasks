4f90d938a5d536d9b8bae0df1d2cf6fd2efbd8fe
= <katoatd@gmail.com>
2018-12-16 14:08:02 +0200

    lecture 04

---
+++ task04/task0412/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task04.task0412;
+
+/* 
+Положительное и отрицательное число
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sNum = reader.readLine();
+        int num = Integer.parseInt(sNum);
+
+        if (num > 0)
+            System.out.println(num * 2);
+        else if (num < 0)
+            System.out.println(num + 1);
+        else
+            System.out.println(0);
+    }
+
+}
