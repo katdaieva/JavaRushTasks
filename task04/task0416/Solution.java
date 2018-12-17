0ac782a100a161b2970f536e92a47e80c3e7d730
= <katoatd@gmail.com>
2018-12-17 23:43:40 +0200

    lecture 04

--- task04/task0416/Solution.java
+++ task04/task0416/Solution.java
@@ -9,6 +9,15 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
-        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sTime = reader.readLine();
+        double nTime = Double.parseDouble(sTime);
+
+        if (nTime % 5 >= 0 && nTime % 5 < 3)
+            System.out.println("зелёный");
+        else if (nTime % 5 >= 3 && nTime % 5 < 4)
+            System.out.println("жёлтый");
+        else
+            System.out.println("красный");
     }
 }
